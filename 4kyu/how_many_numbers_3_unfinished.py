ac912042f46a9ab4d76f6424692ffe3ce4902ba2
Eric Lindesay <psyel1@nottingham.ac.uk>
2022-06-07 22:15:41 +0100

    Added comments on how_many_numbers_3

--- 4kyu/how_many_numbers_3_unfinished.py
+++ 4kyu/how_many_numbers_3_unfinished.py
@@ -3,19 +3,27 @@
 
 import math
 
+from sklearn import multiclass
+
 def find_all(sum_dig, digs):
     if sum_dig > digs*9: # it's impossible to reach the number
+        return []
+    if sum_dig < digs:
         return []
     
     ans = []
     lower_bound = int("1"*(digs))
-    upper_bound = 10**(digs-1)*(math.ceil(sum_dig/digs)+1)
+    upper_bound = int(str(math.ceil(sum_dig/digs))*digs)
+
     if upper_bound < lower_bound:
-        upper_bound = lower_bound + 1
+        upper_bound = lower_bound
 
     print(lower_bound, upper_bound)
 
-    for i in range(lower_bound, upper_bound):
+    # don't use a for loop. Since we know the numbers must be ascending
+    # we can skip a large portion of them.
+    # Do it recursively and you don't need digits_ascending
+    for i in range(lower_bound, upper_bound + 1):
         if digits_ascending(i):
             val = sum(int(x) for x in str(i))
             if val == sum_dig:
@@ -30,5 +38,5 @@ def digits_ascending(num: int) -> bool:
     return str(num) == ''.join(sorted(str(num)))
 
 
-print(find_all(27, 3)) # [8, 118, 334]
+print(find_all(3, 4)) # [8, 118, 334]
 # print(find_all(10, 2))
