3e713eae17325f34fe5e9d722e8b2a2c488d5197
Eric Lindesay <psyel1@nottingham.ac.uk>
2022-06-10 13:10:21 +0100

    strip_comments

--- random_tests.py
+++ random_tests.py
@@ -2,4 +2,4 @@ import random
 
 with open("testfile", 'a') as f:
     for i in range(50):
-        f.write(f"\n{random.randint(20, 65)},{random.randint(2, 9)}")
+        f.write(f"\n{random.randint(20, 65)}|{random.randint(2, 9)}")
